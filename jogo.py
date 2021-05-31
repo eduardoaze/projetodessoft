ba031e322f952b2ae8cb0a07a929180abf019582
eduardoaze <zetoneeduardo@gmail.com>
2021-05-31 16:02:53 -0300

    sem divisão

--- jogo.py
+++
@@ -1,4 +0,0 @@
-import pygame
-from config import FPS, WIDTH, HEIGHT, BLACK, YELLOW, RED
-from assets import load_assets, DESTROY_SOUND, BOOM_SOUND, BACKGROUND, SCORE_FONT
-from sprites import Ship, Meteor, Bullet, Explosion
